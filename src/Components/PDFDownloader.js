f4e7fe7ffaccacb308199b1661de350caa9d61e8
NaveenYadav <yn.naveen00@gmail.com>
2024-03-19 18:20:21 +0530

    Download feature added

--- src/Components/PDFDownloader.js
+++ src/Components/PDFDownloader.js
@@ -3,7 +3,17 @@ import React from 'react';
 const PDFDownloader = () => {
   const downloadPDF = async () => {
     try {
-      const response = await fetch('http://3.110.134.173:8080/pdf'); // Change the URL to your server URL
+      const response = await fetch('http://3.110.134.173:8080/pdf', {
+        method: "GET", // *GET, POST, PUT, DELETE, etc.
+        // mode: "cors", // no-cors, *cors, same-origin
+        // cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+        // credentials: "same-origin", // include, *same-origin, omit
+        headers: {
+          "Content-Type": "application/pdf",
+          "Content-Disposition": "attachment;filename=tasks.pdf"
+          // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      });
       const blob = await response.blob();
       const url = window.URL.createObjectURL(new Blob([blob]));
       const link = document.createElement('a');
